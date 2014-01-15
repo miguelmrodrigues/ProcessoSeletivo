f614a11733f93a1eec35a7621bf8e60d70d269ae
Miguel Rodrigues <rodriguesmmiguel@gmail.com>
2014-01-15 13:49:36 -0200

    Repositorios
    Ainda não implementados

---
+++ ProcessoSeletivo/Models/Domain/Repositories/CategoryRepository.cs
@@ -0,0 +1,32 @@
+﻿using NHibernate;
+using ProcessoSeletivo.Models.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProcessoSeletivo.Models.Domain.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public Category FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Category> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Category> FindAllSubcategories(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Category obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProcessoSeletivo/Models/Domain/Repositories/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProcessoSeletivo.Models.Domain.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public IList<Entities.Product> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Entities.Product> FindAllByCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Entities.Product obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
