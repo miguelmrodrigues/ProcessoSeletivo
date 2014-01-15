bb827c95d5cf0f598cd53b29692dae5a7207a172
Miguel Rodrigues <rodriguesmmiguel@gmail.com>
2014-01-15 12:52:53 -0200

    NHibernate e Entidades
    Adicionado a biblioteca NHibernate e criados as entidades do projeto.

---
+++ ProcessoSeletivo/Models/Domain/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ProcessoSeletivo.Models.Domain.Entities
+{
+    public class Category 
+    {
+        public virtual int id { get; set; }        
+        public virtual string Name { get; set; }
+        public virtual Category ParentCategory { get; set; }
+        public virtual IList<Category> ChildCategories { get; set; }
+        public virtual IList<Product> Products { get; set; }
+    }
+}
---
+++ ProcessoSeletivo/Models/Domain/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ProcessoSeletivo.Models.Domain.Entities
+{
+    public class Product
+    {
+        public virtual int id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual IList<Category> Categories { get; set; }
+    }
+}
