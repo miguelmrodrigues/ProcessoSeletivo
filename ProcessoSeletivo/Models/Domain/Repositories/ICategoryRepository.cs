4c764308940e6cd282b9d2f99c3d1741ca9d14db
Miguel Rodrigues <rodriguesmmiguel@gmail.com>
2014-01-15 13:15:00 -0200

    Mapeamento Nhibernate e Interface dos Repositorios

---
+++ ProcessoSeletivo/Models/Domain/Repositories/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using ProcessoSeletivo.Models.Domain.Entities;
+using System.Collections.Generic;
+
+namespace ProcessoSeletivo.Models.Domain.Repositories
+{
+    public interface ICategoryRepository
+    {
+        public Category FindById(int id);
+
+        public IList<Category> FindAll();
+
+        public IList<Category> FindAllSubcategories(int id);
+
+        public void Save(Category obj);
+    }
+}
---
+++ ProcessoSeletivo/Models/Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using ProcessoSeletivo.Models.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessoSeletivo.Models.Domain.Repositories
+{
+    public interface IProductRepository
+    {
+        public IList<Product> FindAll();
+
+        public IList<Product> FindAllByCategory(int id);
+
+        public void Save(Product obj);
+    }
+}
